9da7c4b0f56ba03ac6ff35ac99e36ae6e14d8b76
Waayway <thijs@vanwaaij.com>
2022-06-06 16:51:58 +0200

    create tables is now only called when starting the server not when import app

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import uvicorn
 import users
 from database import get_db, create_tables
 
-create_tables()
 db = get_db()
 
 app = FastAPI()
@@ -27,4 +26,6 @@ def ToDocs():
 app.include_router(users.router)
 
 if __name__ == "__main__":
+    create_tables()
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    
